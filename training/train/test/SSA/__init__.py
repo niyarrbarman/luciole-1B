783f263cb2bb8a782f92e5c60fe0d3510d64a59a
Niyar R Barman <barmanniyar@gmail.com>
2026-03-10 02:55:05 +0100

    restructured code

--- training/train/test/SSA/__init__.py
+++
@@ -1,6 +0,0 @@
-# SSA: Softmax-Substituted Attention
-# Implementations:
-#   ssa_attention.py              - Original unfused (PyTorch ops)
-#   ssa_flex_attention.py         - FlexAttention-based (PyTorch flex_attention)
-#   ssa_triton_attention.py       - Fused Triton FlashAttention v2/v3
-#   ssa_triton_v4_attention.py    - Fused Triton FlashAttention v4 (tutorial-based structure)

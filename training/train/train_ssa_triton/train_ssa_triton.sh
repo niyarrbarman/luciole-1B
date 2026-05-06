7ef946149815b20f0056fe892caf09b60c5eb4d8
barman <barman@kairoslogin2.calmip.cluster>
2026-05-06 13:45:37 +0200

    logs

--- training/train/train_ssa_triton/train_ssa_triton.sh
+++ training/train/train_ssa_triton/train_ssa_triton.sh
@@ -4,11 +4,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4
 #SBATCH -p full-gpu
-#SBATCH --time=8:00:00
+#SBATCH --time=3:30:00
 #SBATCH --output=slurm/%x_%j.out
 #SBATCH --mail-user=niyar-r.barman@utoulouse.fr
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=288
+##SBATCH --reservation=MC_jeudi
 
 module purge
 mkdir -p slurm
